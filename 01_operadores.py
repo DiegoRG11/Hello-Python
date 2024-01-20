508f7988fc0bd9061fb49199a87cbde28e141b2f
DiegoRG11 <diisaac.36@gmail.com>
2024-01-20 11:47:46 -0400

    Tema 2: Operadores

---
+++ 01_operadores.py
@@ -0,0 +1,30 @@
+# Operadores Aritmeticos
+
+print (3 + 4)
+print (3 - 4)
+print (3 * 4)
+print (10 % 3)
+print (10 // 3)
+print (2 ** 3)
+
+print("Hola " + "Python " + "Que tal?")
+print("Hola " + str(5))
+print("Hola " * 5)
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+# Operadores comparativos
+
+print(3 > 4)
+print(3 < 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 == 4)
+print(3 != 4)
+
+# Operadores Logicos
+
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+# print(3 > 4 not "Hola" > "Python")
